2a5b6bb0e8869a4b7575bec86519926b757b807c
Bilal Yassine <bilal@bilals-mbp.mynetworksettings.com>
2023-10-27 15:18:58 -0400

    Add skeletons for nav bar pages

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,28 +1,17 @@
 import {motion} from 'framer-motion'
-import { useState } from "react"
+import NavLink from './NavLink'
 
 // Nav will be the component to contain the header navigation at the top - Avatar, Title, and Hamburger Nav Icon
 export default function Nav() {
-    const [toggled, setToggled] = useState(false)
-
     return (
         <nav className="relative mx-8 flex justify-between items-center pt-6 pb-4 font-medium md:mx-16 lg:mx-32">
-            {/* Image */}
-            <div>
-                <img src="/images/wizguy.png" alt="Wizguy Picture" width="62" height="72"/>
-            </div>
+            {/* Home */}
+            <NavLink page='Home'/>
 
-            {/* Links */}
-            <a class="font-montserrat text-neutral-100" href="/games">Games</a>
-            <a class="font-montserrat text-neutral-100" href="/blog">Blog</a>
-            <a class="font-montserrat text-neutral-100" href="/about">About</a>
-            
-            {/* Toggle */}
-            <div onClick={() => setToggled((prevToggle) => !prevToggle)} className="space-y-1 cursor-pointer">
-                <span className="block h-0.5 w-8 bg-white"></span>
-                <span className="block h-0.5 w-8 bg-white"></span>
-                <span className="block h-0.5 w-8 bg-white"></span>
-            </div>
+            {/* Other Pages */}
+            <NavLink page='Games'/>
+            <NavLink page='Blog'/>
+            <NavLink page='About'/>
         </nav>
     )
 }
---
+++ src/components/NavLink.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+export default function NavLink(obj: string) {
+    const [toggled, setToggled] = useState(false)
+
+    let link = "/"
+    let image = "/images/wizguy.png"
+    let altText = "WizGuy Picture"
+    let text = ""
+    let width = "62"
+
+    if (obj["page"] == 'Games') {
+        link = "/games"
+        image = "/images/gaming-icon.png"
+        altText = "Games Icon"
+        text = "Games"
+        width = "62"
+    }
+    else if (obj["page"] == 'Blog') {
+        link = "/blog"
+        image = "/images/blog-icon.png"
+        altText = "Blog Icon"
+        text = "Blog"
+        width = "52"
+    }
+    else if (obj["page"] == 'About') {
+        link = "/about"
+        image = "/images/about-icon.png"
+        altText = "About Icon"
+        text = "About"
+        width = "48"
+    }
+
+    return (
+        <a class="font-montserrat text-neutral-100" href={link} >
+            <div>
+                <img src={image} alt={altText} width={width} height="72"/>
+                {text}
+            </div>
+        </a>
+    )
+}

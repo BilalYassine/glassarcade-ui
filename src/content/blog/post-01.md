946aa7562912ae3159bb1040b5af4c019206bc71
BilalYassine <mail@bilalyassine.com>
2024-07-22 12:27:39 +0300

    Website refresh + Blog setup

---
+++ src/content/blog/post-01.md
@@ -0,0 +1,53 @@
+---
+draft: false
+title: "Indie Spotlight: The Messenger"
+snippet: "An absolute gem of the retro-homage indie renaissance."
+image: {
+    src: "/assets/blog/themessenger.jpg",
+    alt: "blog details"
+}
+publishDate: "2024-07-19 11:39"
+category: "Indie Spotlight"
+author: "Bilal Yassine"
+bigImg: {
+    src: "/assets/blog/themessenger.jpg",
+    alt: "blog details",
+}
+authorImg: {
+    src: "/assets/team/bibo.png",
+    alt: "author",
+}
+comments: "00"
+views: "00"
+tags: [Indie, Review, Design]
+postDetails: {
+    paraOne: "These days, the gaming world is moving at neck-breaking speeds, with massive breathtaking environments and cinematic campaigns at the forefront of AAA flagship releases – and rightfully so – modern hardware has gotten so good, it only makes sense for the industry to be pushing its capabilities to its absolute limits both physically and artistically. 
+
+    \n \n
+    
+    And yet! A peek into the indie sphere shows the people’s allure for the classics. We are currently living in a retro-style renaissance of indie titles, where developers are using their limited resources to put together beautiful homages to the retro gaming of the past. Some of the biggest hits and indie darlings — Shovel Knight, Stardew Valley, Celeste, and Animal Well, to name a few — embrace the pixel art form and limited color palettes that defined the worlds of yore.",
+    paraTwo: "But one that I think stands above the rest in this mass revival of retro games is The Messenger. Sabotage Studio captures the essence of the Ninja Gaiden games of the past while also brilliantly defying expectations and reinventing their game into one unlike any I have played before.",
+    title: "The Messenger - a gem of the retro-homage renaissance",
+    paraThree: "Right off the bat, there is so much to love about this game.
+
+    \n \n
+    
+    The Messenger is a 2D action platformer which sets itself up as a fast-paced, 8-bit arcade platformer. You, as “the Messenger,” quickly navigate through platforming challenges using simple but fluid mechanics like the Cloud Step. Unlike its inspirations, the game pushes you forward with an engaging, consistent pace and generous respawn forgiveness. It also has a great sense of humor: the shopkeeper’s quips, sometimes surprisingly friendly bosses, and meta dialogue kept me smiling for most of my playthrough and really showed off the game’s personality.
+",
+    titleTwo: "An 8-bit/16-bit Split - Two Games in One",
+    paraFour: "Then, a couple hours into the game, it changes. Suddenly, the familiar 8-bit adventure the gamer has been on evolves into a 16-bit expedition through time. Here, you move through the world with a new ability to shift between past and future, old and new, 8-bit and 16-bit. Not only is this a clever visual representation of time, but it also gives the developers a chance to show off their artistic brilliance. Seriously, this is some of the best pixel art I’ve seen in a platformer.
+
+    \n \n
+
+    The developers also use the 8-bit/16-bit split to flex the excellence in their game design. Using the time portals to switch between art styles is much more than just a graphical gimmick and completely re-invents each level. Obstacles change, pathways are altered, and the same game assets are cleverly repurposed in new ways to keep things fresh. For example, the 16-bit version of the Cloud Ruins has us playing through an ancient ruinous area, but going back to its 8-bit version transforms the level into its peak as a bustling thunderous temple.
+
+    \n \n
+    
+    While moving through a section, I found myself simultaneously having a blast and being utterly impressed by its design. This amazing hybrid feeling returned many times as I played the game — “Wow, this is fun,” and “Wow, this is impressive,” were two thoughts that often went hand-in-hand throughout my experience.",
+    paraFive: "Although it can be easy to see the influx of retro-style indie games as excuses for developers to settle (“Why make a NEW new game when I make an OLD new game?”), gems like The Messenger highlight just how exciting they can be. Retro homage games are at their best when you can feel the love of what came before and use it to completely subvert a gamer’s expectations. The Messenger expertly shows that nostalgia can be a clever tool to create something groundbreaking and brilliantly new.",
+}
+quotes : {
+    quote: "",
+    author: ""
+}
+---
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,28 +1,157 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: "class",
 	theme: {
-		extend: {
-			fontFamily: {
-				'poppins': ['Poppins']
-			},
-			colors:{
-				'primaryBlue': '#88D3E1',
-				'primaryRed': '#F04E50',
-				'primaryWhite': '#FFFFFF',
-				'primaryBlack': '#151715',
-				'primaryNavy': '#232C47',
-				'backgroundBlack': '#131313'
-			},
-			spacing: {
-				'2/3': '66.66667%',
-				'7/12': '58.333333%',
-				'1/2': '50%'
-			}
-		},
-		screens:{
-			'lg' : '801px'
-		}
-	},
-	plugins: [],
+    screens: {
+      sm: "540px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "720px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "960px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1140px",
+      // => @media (min-width: 1200px) { ... }
+
+      "2xl": "1320px",
+      // => @media (min-width: 1400px) { ... }
+    },
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        lightblack: "#27262a",
+        white: "#FFFFFF",
+        black: "#27262a",
+        "dark-700": "#090e34b3",
+        blue: {
+          DEFAULT: "#88D3E1",
+          2: "#5ba7b4",
+          3: "#2c7d8a",
+          4: "#005561",
+          5: "#00303c",
+          6: "#9CA3AF",
+          7: "#D1D5DB",
+          8: "#E5E7EB",
+        },
+        red: {
+          DEFAULT: "#F04E50",
+          2: "#cc2a36",
+          3: "#a8001e",
+          4: "#850003",
+          5: "#640000",
+          6: "#9CA3AF",
+          7: "#D1D5DB",
+          8: "#E5E7EB",
+        },
+        light: {
+          DEFAULT: "#CC2A36",
+          2: "#cc2a36",
+          3: "#a8001e",
+          4: "#850003",
+          5: "#640000",
+          6: "#9CA3AF",
+          7: "#D1D5DB",
+          8: "#E5E7EB",
+        },
+        dark: {
+          DEFAULT: "#5BA7B4",
+          2: "#5ba7b4",
+          3: "#2c7d8a",
+          4: "#005561",
+          5: "#00303c",
+          6: "#9CA3AF",
+          7: "#D1D5DB",
+          8: "#E5E7EB",
+        },
+        primaryred: "#F04E50",
+        primaryblue: "#FFFFFF",
+        primary: "#FFFFFF",
+        "blue-dark": "#FFFFFF",
+        secondary: "#FFFFFF",
+        "body-color": "#FFFFFF",
+        "body-secondary": "#FFFFFF",
+        warning: "#FBBF24",
+        stroke: "#FFFFFF",
+        "gray-1": "#FFFFFF",
+        "gray-2": "#FFFFFF",
+        "gray-7": "#FFFFFF",
+      },
+      fontFamily: {
+        rajdhani: ['Rajdhani', 'sans-serif'],
+      },
+      fontWeight: {
+        light: 300,
+        regular: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+      },
+      boxShadow: {
+        input: "0px 7px 20px rgba(0, 0, 0, 0.03)",
+        form: "0px 1px 55px -11px rgba(0, 0, 0, 0.01)",
+        pricing: "0px 0px 40px 0px rgba(0, 0, 0, 0.08)",
+        "switch-1": "0px 0px 5px rgba(0, 0, 0, 0.15)",
+        testimonial: "0px 10px 20px 0px rgba(92, 115, 160, 0.07)",
+        "testimonial-btn": "0px 8px 15px 0px rgba(72, 72, 138, 0.08)",
+        1: "0px 1px 3px 0px rgba(166, 175, 195, 0.40)",
+        2: "0px 5px 12px 0px rgba(0, 0, 0, 0.10)",
+      },
+    },
+  },
+	plugins: [
+    function({ addUtilities }) {
+      const newUtilities = {
+        '.rajdhani-light': {
+          fontFamily: 'Rajdhani, sans-serif',
+          fontWeight: 300,
+        },
+        '.rajdhani-regular': {
+          fontFamily: 'Rajdhani, sans-serif',
+          fontWeight: 400,
+        },
+        '.rajdhani-medium': {
+          fontFamily: 'Rajdhani, sans-serif',
+          fontWeight: 500,
+        },
+        '.rajdhani-semibold': {
+          fontFamily: 'Rajdhani, sans-serif',
+          fontWeight: 600,
+        },
+        '.rajdhani-bold': {
+          fontFamily: 'Rajdhani, sans-serif',
+          fontWeight: 700,
+        },
+        '.image-gradient-border': {
+          position: 'relative',
+          display: 'inline-block',
+        },
+        '.image-gradient-border::before': {
+          content: '""',
+          position: 'absolute',
+          top: '0',
+          left: '0',
+          right: '0',
+          bottom: '0',
+          borderRadius: '8px', // Adjust as needed
+          padding: '4px', // Adjust as needed
+          background: 'linear-gradient(45deg, rgba(255, 0, 0, 0.5), rgba(0, 255, 0, 0.5))',
+          '-webkit-mask': 'linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)',
+          '-webkit-mask-composite': 'xor',
+          'mask-composite': 'exclude', // For Firefox
+          pointerEvents: 'none',
+        },
+      }
+
+      addUtilities(newUtilities, ['responsive', 'hover'])
+    },
+  ],
 }
